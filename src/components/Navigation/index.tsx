3723e2d50675f036752d9b9d9332b3a452b9c951
Robin Rieger <rrieger@e-spirit.com>
2020-09-07 13:50:03 +0200

    TNG-546 added mobile Nav

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -19,6 +19,8 @@ class Navigation extends BaseComponent<NavigationProps> {
   @Prop({ type: Function })
   isActiveItem: NavigationProps["isActiveItem"];
   @Prop({ type: Number, default: 3 }) depth!: number;
+  @Prop({ type: Function })
+  isCollapsed = false;
 
   renderItem(item: NavigationItem, currentDepth: number) {
     const isActive = this.isActiveItem ? this.isActiveItem(item) : false;
@@ -50,8 +52,61 @@ class Navigation extends BaseComponent<NavigationProps> {
         <ul class="Navigation--Navigation hidden lg:block">
           {this.items.map(item => this.renderItem(item, 0))}
         </ul>
-        <div class="Navigation--Mobile w-full flex items-center justify-end lg:hidden hover:text-gray-600 text-xl cursor-pointer">
+        <div
+          class="Navigation--Mobile w-full flex items-center justify-end lg:hidden hover:text-gray-600 text-xl cursor-pointer"
+          onClick={(event: any) => {
+            event?.preventDefault();
+            this.isCollapsed = !this.isCollapsed;
+          }}
+        >
           <i class="fas fa-bars" />
+          <div class={` Mobile ${this.isCollapsed ? "collapsed" : ""}`}>
+            <ul class=" nav navbar-nav">
+              <li>
+                <a href="index.html">Home</a>
+              </li>
+              <li>
+                <a href="security.html">Security</a>
+              </li>
+              <li class="active">
+                <a href="products.html">Products</a>
+                <ul class="sub-menu">
+                  <li>
+                    <a href="category.html">Smart Door Lock</a>
+                  </li>
+                  <li>
+                    <a href="category.html">Motion Detector</a>
+                  </li>
+                  <li>
+                    <a href="category.html">Interior HD Camera</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href="specialized_dealers.html">Specialized Dealers</a>
+              </li>
+              <li>
+                <a href="about-us.html">About Us</a>
+                <ul class="sub-menu">
+                  <li>
+                    <a href="services.html">Services</a>
+                  </li>
+                  <li>
+                    <a href="company.html">Company</a>
+                  </li>
+                  <li>
+                    <a href="faq.html">FAQ</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href="blog.html">Blog</a>
+              </li>
+              <li>
+                <a href="contact.html">Contact</a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     );
