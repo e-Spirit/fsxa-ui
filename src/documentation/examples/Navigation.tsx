0597c9591a761ece915712dfc50adce7b65df4d3
Robin Rieger <rrieger@e-spirit.com>
2020-09-08 16:49:39 +0200

    added styling for mobile Navigation and the 3rd depth

--- src/documentation/examples/Navigation.tsx
+++ src/documentation/examples/Navigation.tsx
@@ -29,21 +29,51 @@ const navItems: NavigationItem[] = [
     id: "2",
     path: "/",
     label: "Link 2",
-    children: [],
+    children: [
+      {
+        id: "5",
+        path: "/",
+        label: "Link 2.1.",
+        children: [],
+      },
+      {
+        id: "6",
+        path: "/",
+        label: "Link 2.2",
+        children: [
+          {
+            id: "6",
+            path: "/",
+            label: "Link 2.1.1",
+            children: [],
+          },
+        ],
+      },
+    ],
   },
 ];
 
 @Component
 export default class App extends Vue {
+  currentItem: any = null;
   render() {
     return (
+<<<<<<< HEAD
       <div class="space-x-5 mb-10 ml-64">
         <Navigation
           isActiveItem={item => item.id === "1"}
           depth={3}
+=======
+      <div class="space-x-5 py-10" style={{ height: "500px" }}>
+        <Navigation
+          isActiveItem={item =>
+            this.currentItem && item.id === this.currentItem.id
+          }
+          depth={2}
+>>>>>>> ab6e45d... added styling for mobile Navigation and the 3rd depth
           items={navItems}
-          handleNavClick={() => {
-            return;
+          handleNavClick={navItem => {
+            this.currentItem = navItem;
           }}
         />
       </div>
