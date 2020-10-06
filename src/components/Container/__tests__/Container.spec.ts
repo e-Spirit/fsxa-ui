a3ff892902174f9163ff5288824cde1b8be0386d
Vlado Plaga <plaga@e-spirit.com>
2020-10-06 10:31:51 +0200

    TNG-775 fixed and added new Container test cases

--- src/components/Container/__tests__/Container.spec.ts
+++ src/components/Container/__tests__/Container.spec.ts
@@ -1,18 +1,27 @@
-import { render, fireEvent, getByTestId } from "@testing-library/vue";
+import { render } from "@testing-library/vue";
 import Container from "./../";
 
 describe("components/Container", () => {
-  it("renders passed content as container content, and has default classes", () => {
+  const defaultClasses = [
+    "px-6",
+    "md:px-10",
+    "lg:px-12",
+    "py-6",
+    "md:py-12",
+    "lg:py-24",
+  ];
+
+  it("renders passed content as container content, and has default class 'container'", () => {
     const content = "This is my Container test";
     const { getByText } = render(Container, {
       slots: { default: content },
     });
     const container = getByText(content);
-    expect(container).toBeTruthy;
-    expect(container.classList.contains("container")).toBeTruthy;
+    expect(container).toBeTruthy();
+    expect(container.classList.contains("container")).toBe(true);
   });
 
-  it("calls handleClick callback on click", async () => {
+  it("has class w-full instead of container when property fluid is set", async () => {
     const { getByTestId } = render(Container, {
       slots: { default: "Content" },
       props: {
@@ -20,7 +29,31 @@ describe("components/Container", () => {
       },
     });
     const container = getByTestId("container");
-    expect(container.classList.contains("w-full")).toBeTruthy;
-    expect(container.classList.contains("container")).toBeFalsy;
+    expect(container.classList.contains("w-full")).toBe(true);
+    expect(container.classList.contains("container")).toBe(false);
+  });
+
+  it("should have a specific list of classes when horizontalPadding and verticalPadding are set (default)", () => {
+    const { getByTestId } = render(Container, {
+      slots: { default: "Some content" },
+    });
+    const containerClasses = getByTestId("container").classList;
+    defaultClasses.forEach(element => {
+      expect(containerClasses.contains(element)).toBe(true);
+    });
+  });
+
+  it("should not have any of the default list of classes when horizontalPadding and verticalPadding are set to false", () => {
+    const { getByTestId } = render(Container, {
+      slots: { default: "Some content" },
+      props: {
+        verticalPadding: false,
+        horizontalPadding: false,
+      },
+    });
+    const containerClasses = getByTestId("container").classList;
+    defaultClasses.forEach(element => {
+      expect(containerClasses.contains(element)).toBe(false);
+    });
   });
 });
