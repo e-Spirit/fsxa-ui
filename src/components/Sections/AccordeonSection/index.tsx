e89dbf876d3da43abbdfeaca76d3afc4ee2a3057
Emre Izgoel <izgoel@e-spirit.com>
2020-10-09 14:12:04 +0200

    finished styling

--- src/components/Sections/AccordeonSection/index.tsx
+++ src/components/Sections/AccordeonSection/index.tsx
@@ -1,6 +1,6 @@
 import { Component, Prop } from "vue-property-decorator";
 import BaseComponent from "@/components/BaseComponent";
-import { AccordeonSectionProps } from "@/types/sections";
+import { AccordeonSectionProps, Accordeon } from "@/types/sections";
 
 @Component({
   name: "AccordeonSection",
@@ -11,7 +11,31 @@ class AccordeonSection extends BaseComponent<AccordeonSectionProps> {
   @Prop({ type: Array, default: [{ title: "title", text: "lorem ipsum" }] })
   items: AccordeonSectionProps["items"];
   render() {
-    return <h1>This is an accordeon</h1>;
+    return (
+      <div class="md p-4">
+        {this.items.map(item => this.renderAccordeon(item))}
+      </div>
+    );
+  }
+  renderAccordeon(item: Accordeon) {
+    const colorClasses = this.dark
+      ? "bg-black text-white"
+      : "bg-transparent text-black";
+    return (
+      <div class="w-full">
+        <div class={colorClasses + " p-2 w-full clearfix"}>
+          <h6 class="float-left w-2/3 whitespace-no-wrap overflow-hidden">
+            {item.title}
+          </h6>
+          <span class="indicator float-right">
+            <i class="fa fa-plus"></i>
+          </span>
+        </div>
+        <div>
+          <p>{item.text}</p>
+        </div>
+      </div>
+    );
   }
 }
 export default AccordeonSection;
---
+++ src/documentation/examples/Sections/AccordeonSection.tsx
@@ -0,0 +1,4 @@
+import Vue from "vue";
+import Component from "vue-class-component";
+
+import { Sections } from "fsxa-ui";
