de43237dfcc663e99cffd1de75fa6e0dc163cb0e
Emre Izgoel <izgoel@e-spirit.com>
2020-10-23 14:54:36 +0200

    fixed animations

--- src/components/Sections/AccordionSection/Accordion/index.tsx
+++ src/components/Sections/AccordionSection/Accordion/index.tsx
@@ -1,4 +1,4 @@
-import { Component, Prop } from "vue-property-decorator";
+import { Component, Prop, Watch } from "vue-property-decorator";
 import BaseComponent from "@/components/BaseComponent";
 import { AccordionProps } from "@/types/components";
 import "./style.css";
@@ -13,9 +13,54 @@ class Accordion extends BaseComponent<AccordionProps> {
   @Prop({ required: true }) index!: AccordionProps["index"];
   @Prop({ default: false }) open!: AccordionProps["open"];
 
+  @Watch("open")
+  toggleOpen(open: boolean): void {
+    const accordion = this.$el.querySelector(
+      ".Accordion--Text-Box",
+    )! as HTMLElement;
+    if (open) {
+      this.openAccordion(accordion);
+    } else {
+      this.closeAccordion(accordion);
+    }
+  }
+
+  closeAccordion(accordion: HTMLElement): void {
+    console.log("closing", this.index);
+    const height = accordion.scrollHeight;
+
+    const transition = accordion.style.transition;
+    accordion.style.transition = "";
+
+    requestAnimationFrame(() => {
+      accordion.style.height = height + "px";
+      accordion.style.transition = transition;
+
+      requestAnimationFrame(() => (accordion.style.height = "0px"));
+    });
+  }
+
+  openAccordion(accordion: HTMLElement): void {
+    console.log("opening", this.index);
+    const height = accordion.scrollHeight;
+    accordion.style.height = height + "px";
+
+    const handleTransitionEnd = (e: TransitionEvent) => {
+      accordion.style.height = "auto";
+    };
+
+    accordion.addEventListener(
+      "transitionend",
+      (e: TransitionEvent) => {
+        handleTransitionEnd(e);
+      },
+      { once: true },
+    );
+  }
+
   render() {
     return (
-      <div class={"Accordion" + `${this.open ? " Accordion--Open" : ""}`}>
+      <div class={`Accordion ${this.open ? "Accordion--Open" : ""}`}>
         <div
           class={`${
             this.dark ? "Accordion--Dark" : "Accordion--Light"
