958f66e375853f24e9c85d5be4637e536f084074
Robin Rieger <rrieger@e-spirit.com>
2020-10-26 10:22:04 +0100

    init example

--- src/documentation/examples/ProductDetailSection.tsx
+++ src/documentation/examples/ProductDetailSection.tsx
@@ -1,14 +1,13 @@
 import Vue from "vue";
 import Component from "vue-class-component";
-
-import ProductDetailSection from "fsxa-ui";
+import { Sections } from "fsxa-ui";
 
 @Component
 export default class App extends Vue {
   render() {
     return (
       <div class="space-x-5 mb-10 ml-64">
-        <ProductDetailSection></ProductDetailSection>
+        <Sections.ProductDetailSection></Sections.ProductDetailSection>
       </div>
     );
   }
