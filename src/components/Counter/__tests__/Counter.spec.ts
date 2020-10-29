7782a468f5df24faea360891ea6e5abc650b9492
Vlado Plaga <plaga@e-spirit.com>
2020-10-29 15:53:48 +0100

    TNG-776 fixed typo

--- src/components/Counter/__tests__/Counter.spec.ts
+++ src/components/Counter/__tests__/Counter.spec.ts
@@ -2,7 +2,7 @@ import { render } from "@testing-library/vue";
 import Counter from "./../";
 
 describe("components/Counter", () => {
-  it("should render counter with both properties, value and label, in excluse html elements", () => {
+  it("should render counter with both properties, value and label, in exclusive html elements", () => {
     const counterValue = "23";
     const counterLabel = "my counter";
     const { getByText } = render(Counter, {
