2271362e3cf95066be8a563c4f42f2386b9b37c5
Vlado Plaga <plaga@e-spirit.com>
2020-10-29 15:37:34 +0100

    TNG-775 removed two tests, no lint problems

--- src/components/Container/__tests__/Container.spec.ts
+++ src/components/Container/__tests__/Container.spec.ts
@@ -2,15 +2,6 @@ import { render } from "@testing-library/vue";
 import Container from "./../";
 
 describe("components/Container", () => {
-  const defaultClasses = [
-    "px-6",
-    "md:px-10",
-    "lg:px-12",
-    "py-6",
-    "md:py-12",
-    "lg:py-24",
-  ];
-
   it("renders passed content as container content, and has default class 'container'", () => {
     const content = "This is my Container test";
     const { getByText } = render(Container, {
@@ -31,29 +22,5 @@ describe("components/Container", () => {
     const container = getByTestId("container");
     expect(container.classList.contains("w-full")).toBe(true);
     expect(container.classList.contains("container")).toBe(false);
-  });
-
-  it("should have a specific list of classes when horizontalPadding and verticalPadding are set (default)", () => {
-    const { getByTestId } = render(Container, {
-      slots: { default: "Some content" },
-    });
-    const containerClasses = getByTestId("container").classList;
-    defaultClasses.forEach(element => {
-      expect(containerClasses.contains(element)).toBe(true);
-    });
-  });
-
-  it("should not have any of the default list of classes when horizontalPadding and verticalPadding are set to false", () => {
-    const { getByTestId } = render(Container, {
-      slots: { default: "Some content" },
-      props: {
-        verticalPadding: false,
-        horizontalPadding: false,
-      },
-    });
-    const containerClasses = getByTestId("container").classList;
-    defaultClasses.forEach(element => {
-      expect(containerClasses.contains(element)).toBe(false);
-    });
   });
 });
