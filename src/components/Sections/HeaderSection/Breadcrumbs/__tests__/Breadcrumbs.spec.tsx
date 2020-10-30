8e0a4779b0f90db91d7641784f73b91863b2d18c
Robin Rieger <rrieger@e-spirit.com>
2020-10-30 13:23:11 +0100

    added new functions to tests

--- src/components/Sections/HeaderSection/Breadcrumbs/__tests__/Breadcrumbs.spec.tsx
+++ src/components/Sections/HeaderSection/Breadcrumbs/__tests__/Breadcrumbs.spec.tsx
@@ -34,8 +34,46 @@ describe("components/Breadcrumbs", () => {
     const breadcrumb = container.querySelector(
       ".Breadcrumbs--Item [data-testid='item-1']",
     );
-    console.log(breadcrumb);
     await fireEvent(breadcrumb!, new Event("click"));
     expect(spy).toHaveBeenCalled();
+    expect(spy.mock.calls[0][0]).toEqual({
+      referenceId: "1",
+      referenceType: "test",
+      path: "/",
+      label: "Link 1",
+    });
+  });
+  it("checks if all items have been rendered", async () => {
+    const spy = jest.fn();
+    const items = [
+      {
+        referenceId: "1",
+        referenceType: "test",
+        path: "/",
+        label: "Link 1",
+      },
+      {
+        referenceId: "2",
+        referenceType: "test",
+        path: "/",
+        label: "Link 2",
+      },
+      {
+        referenceId: "3",
+        referenceType: "test",
+        path: "/",
+        label: "Link 3",
+      },
+    ];
+    const { container } = render(Breadcrumbs, {
+      slots: { default: "Content" },
+      props: {
+        handleItemClick: spy,
+        items: items,
+      },
+    });
+    items.forEach(item => {
+      expect(item.label).toBeTruthy();
+    });
   });
 });
