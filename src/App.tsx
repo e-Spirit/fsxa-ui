2028f2ffd710b20c4334b7c4701bc8d411a39275
Nino Nato <nato@e-spirit.com>
2020-11-11 16:38:40 +0100

    TNG-731 Updated demo in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,56 @@
 import { Component } from "vue-property-decorator";
 import Vue from "vue";
-import Documentation from "./documentation";
-import { ProductDetailItem } from "@/types/sections";
+import { ProductProperty } from "@/types/sections";
 import ProductDetailSection from "./components/Sections/ProductDetailSection";
 
 const description =
   "The Smart Door Lock SRT-456 is a mechanical look which can be unlocked with a PIN or your smartphone. In addition you can use the camera with integrated face recognition to allow only certain people to unlock the door.";
 const headline = "Smart Door Lock SRT-456";
 const price = "$ 239 (RLP)";
-const src =
+const src1 =
   "https://enterprisedev.e-spirit.cloud/media/img/Bötzow-Brauerei_Gelände_1900.jpg";
-const button = "search spezialized Dealer";
-const headline2 = "Categories";
-const headline3 = "Compatibilities";
+const src2 =
+  "https://enterprisedev.e-spirit.cloud/smartlivingglobal/Images/Product-Images/Smart-door-lock-round_product_teaser.jpg";
+const button = "search specialized Dealer";
 
-const categories: ProductDetailItem[] = [
+const propertyList: ProductProperty[] = [
   {
-    id: "1",
-    label: "Door Locks",
+    title: "Categories",
+    properties: [
+      {
+        id: "1",
+        name: "Door Locks",
+      },
+    ],
+  },
+  {
+    title: "Compatibilities",
+    properties: [
+      {
+        id: "1",
+        name: "Google Home",
+      },
+      {
+        id: "2",
+        name: "Amazon Alexa",
+      },
+      {
+        id: "3",
+        name: "Bosch Smart Home",
+      },
+    ],
   },
 ];
-const compatibility: ProductDetailItem[] = [
-  {
-    id: "1",
-    label: "Google Home",
-  },
-  {
-    id: "2",
-    label: "Amazon Alexa",
-  },
-  {
-    id: "3",
-    label: "Bosch Smart Home",
-  },
-];
+
+const foldableContentList: Record<string, string> = {
+  Delivery: `<ul class="list-disc px-10 py-4"><li>mechanical lock</li><li>camera with integrated face recognition</li><li>wireless numpad</li><li>operation manual</li></ul>`,
+  "Installation instructions": "<p>Fix it at the door. Ready.</p>",
+  Compatibilities: `<ul class="list-disc px-10 py-4"><li>Google Home</li><li>Amazon Alexa</li><li>Bosch Smart Home</li></ul>`,
+};
+
+function handleClick() {
+  console.log(`Clicked on `);
+}
 
 @Component
 class App extends Vue {
@@ -43,12 +60,14 @@ class App extends Vue {
         description={description}
         headline={headline}
         price={price}
+        handleButtonClick={handleClick}
         buttonText={button}
-        image={{ src: src, dimensions: { width: 5, height: 2 } }}
-        compatibilities={compatibility}
-        categories={categories}
-        compatibilityHeadline={headline3}
-        categoryHeadline={headline2}
+        images={[
+          { src: src1, dimensions: { width: 5, height: 2 } },
+          { src: src2, dimensions: { width: 5, height: 2 } },
+        ]}
+        propertyList={propertyList}
+        foldableContentList={foldableContentList}
       />
     );
   }
