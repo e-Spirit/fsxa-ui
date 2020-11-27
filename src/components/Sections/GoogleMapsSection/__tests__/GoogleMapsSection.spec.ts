fe66f4e21c2e2e8046f884b3c513d50d151973ae
Emre Izgoel <izgoel@e-spirit.com>
2020-11-27 13:09:49 +0100

    googlemaps-section 🖥️wip: TNG-732 added test for language validation

--- src/components/Sections/GoogleMapsSection/__tests__/GoogleMapsSection.spec.ts
+++ src/components/Sections/GoogleMapsSection/__tests__/GoogleMapsSection.spec.ts
@@ -1,6 +1,7 @@
 import { render } from "@testing-library/vue";
 import "@testing-library/jest-dom";
 import GoogleMapsSection from "..";
+import { GoogleMapsSectionProps } from "../../../../types/sections";
 
 const apikey = process.env.VUE_APP_MAPS_APIKEY;
 
@@ -15,21 +16,38 @@ const locations = [
     street: "Skodagasse 13",
   },
 ];
+// eslint-disable-next-line
+const handleButtonClick = () => {}
+const requiredProps = {
+  apikey,
+  buttonLabel: "label",
+  language: "en",
+  handleButtonClick,
+} as GoogleMapsSectionProps;
 
 describe("components/sections/google-maps-section", () => {
   describe("given a title prop", () => {
-    const props = {
-      apikey,
-      title: "This is a title",
-      locations,
-      // eslint-disable-next-line
-      handleButtonClick: () => {},
-    };
+    const props = requiredProps;
+    props.title = "This is a title";
     it("should render a title", () => {
       const { getByText } = render(GoogleMapsSection, {
         props,
       });
       expect(getByText("This is a title")).toBeInTheDocument();
+    });
+  });
+  describe("given a language prop", () => {
+    it("should throw an error if the length of language string is not exactly 2", () => {
+      const longProps = requiredProps;
+      longProps.language = "eng";
+      const shortProps = requiredProps;
+      shortProps.language = "e";
+      expect(() =>
+        render(GoogleMapsSection, { props: longProps }),
+      ).toThrowError();
+      expect(() =>
+        render(GoogleMapsSection, { props: shortProps }),
+      ).toThrowError();
     });
   });
 });
