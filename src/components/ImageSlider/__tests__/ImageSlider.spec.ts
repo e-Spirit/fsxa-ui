069ebc493d2a4dc69829d400d8c4fde7406caf87
Nino Nato <nato@e-spirit.com>
2021-01-14 11:07:09 +0100

    Add tests for ImageSlider

--- src/components/ImageSlider/__tests__/ImageSlider.spec.ts
+++ src/components/ImageSlider/__tests__/ImageSlider.spec.ts
@@ -1,7 +1,7 @@
 import { render, fireEvent } from "@testing-library/vue";
 import ImageSlider from "..";
 
-const IMAGES = [
+const IMAGES_URLS = [
   "https://images.unsplash.com/photo-1504618223053-559bdef9dd5a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
   "https://images.unsplash.com/photo-1549799521-b6b4c1aaf2ec?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1949&q=80",
   "https://images.pexels.com/photos/4065624/pexels-photo-4065624.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
@@ -11,26 +11,26 @@ describe("components/ImageSlider", () => {
   it("renders passed images as image slider", () => {
     const { container, getByTestId } = render(ImageSlider, {
       props: {
-        images: IMAGES.map(url => ({ src: url })),
+        images: IMAGES_URLS.map(url => ({ src: url })),
       },
     });
     expect(container).toBeTruthy();
-    IMAGES.forEach((image, index) => {
+    IMAGES_URLS.forEach((image, index) => {
       const imageElement = getByTestId(`image-${index}`);
       expect(imageElement).toBeTruthy();
       const nativeImgElement = imageElement.querySelector("img");
       expect(nativeImgElement?.src).toEqual(image);
     });
     const buttonPrevSlide = getByTestId("button-prev-slide");
-    const buttonNextSlide = getByTestId("button-next-slide");
     expect(buttonPrevSlide).toBeTruthy();
+    const buttonNextSlide = getByTestId("button-next-slide");
     expect(buttonNextSlide).toBeTruthy();
   });
 
   it("should switch the displayed image", async () => {
     const { getByTestId } = render(ImageSlider, {
       props: {
-        images: IMAGES.map(url => ({ src: url })),
+        images: IMAGES_URLS.map(url => ({ src: url })),
       },
     });
     const image = getByTestId("image-0");
@@ -39,9 +39,9 @@ describe("components/ImageSlider", () => {
     expect(imageContainerParent).toBeTruthy();
     expect(imageContainerParent?.style.transform).toEqual("translateX(-0%)");
     const buttonNextSlide = getByTestId("button-next-slide");
-    const buttonPrevSlide = getByTestId("button-prev-slide");
     await fireEvent(buttonNextSlide, new Event("click"));
     expect(imageContainerParent?.style.transform).toEqual("translateX(-100%)");
+    const buttonPrevSlide = getByTestId("button-prev-slide");
     await fireEvent(buttonPrevSlide, new Event("click"));
     expect(imageContainerParent?.style.transform).toEqual("translateX(-0%)");
   });
@@ -49,7 +49,7 @@ describe("components/ImageSlider", () => {
   it("should replace the default rendering of the image when the scopedSlot is being used", () => {
     const { getByTestId, container } = render(ImageSlider, {
       props: {
-        images: IMAGES.map(url => ({ src: url })),
+        images: IMAGES_URLS.map(url => ({ src: url })),
       },
       scopedSlots: {
         image: `<img class="scoped-slot-image" src="imageSrc" />`,
