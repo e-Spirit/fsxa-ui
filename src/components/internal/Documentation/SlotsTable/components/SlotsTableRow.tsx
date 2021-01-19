130364dc8b693300bdf507bc25850a1ec16d42a7
Emre Neumann <emre.izgoel@gmail.com>
2021-01-19 12:34:57 +0100

    feat: Slotify List section
    Replaced renderItem prop with mandatory item scoped slot.
    Added optional filter scoped slot to allow for custom rendering of the filter buttons.

--- src/components/internal/Documentation/SlotsTable/components/SlotsTableRow.tsx
+++ src/components/internal/Documentation/SlotsTable/components/SlotsTableRow.tsx
@@ -4,6 +4,7 @@ import { Component, Prop } from "vue-property-decorator";
 export interface SlotsTableRowProps {
   value: string;
   prop: string;
+  required: boolean;
 }
 @Component({
   name: "SlotsTableRow",
@@ -11,12 +12,17 @@ export interface SlotsTableRowProps {
 class SlotsTableRow extends BaseComponent<SlotsTableRowProps> {
   @Prop({ required: true }) value!: SlotsTableRowProps["value"];
   @Prop({ required: false }) prop!: SlotsTableRowProps["prop"];
+  @Prop() required!: SlotsTableRowProps["required"];
 
   render() {
     return (
       <tr>
         <td>
-          <span class={`py-1 px-2 "bg-gray-200" w-auto rounded-lg text-sm`}>
+          <span
+            class={`py-1 px-2 ${
+              this.required ? "bg-gray-900 text-white" : "bg-gray-200"
+            } w-auto rounded-lg text-sm`}
+          >
             {this.value}
           </span>
         </td>
--- src/types/sections.d.ts
+++ src/types/sections.d.ts
@@ -12,10 +12,28 @@ export interface NewsTeaserSectionProps {
   handleItemClick: (item: NewsTeaserItemProps) => void;
 }
 export class NewsTeaserSection extends Component<NewsTeaserSectionProps> {}
-
+export interface ListSectionSlots<Item> {
+  /**
+   * This slot acts as a template for how the items should be displayed
+   */
+  item: Item;
+  /**
+   * You can choose to render your own headline
+   */
+  headline?: string;
+  /**
+   * This slot acts as a template for how the filters should be displayed
+   */
+  filter?: {
+    key: string;
+    value: any;
+    selected: boolean;
+    handleClick: (key: string) => void;
+  };
+}
 export interface ListSectionProps<Item> {
   /**
-   * You can pass an optional headline that will be displayed in top of your items.
+   * You can pass an optional headline that will be displayed above your items.
    * An additional separator will be rendered as well.
    */
   headline?: string;
@@ -23,10 +41,6 @@ export interface ListSectionProps<Item> {
    * The items that should be iterated over
    */
   items: Item[];
-  /**
-   * Render-Prop with which you must define, how your items should get rendered
-   */
-  renderItem: (item: Item) => JSX.Element | JSX.Element[] | null;
   /**
    * Optional filters that will be displayed between headline and items
    */
