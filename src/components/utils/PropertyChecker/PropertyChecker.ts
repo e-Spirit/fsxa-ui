d10c7023385337b7d2cbc4975f4c9f4dbf637d78
Dominik Barczewski <68270882+dbarczewski@users.noreply.github.com>
2021-02-09 12:59:39 +0100

    Update PropertyChecker.ts

--- src/components/utils/PropertyChecker/PropertyChecker.ts
+++ src/components/utils/PropertyChecker/PropertyChecker.ts
@@ -1,9 +1,9 @@
 /**
  * Checks if defined value is part of an array, if not that Element will be removed and an error is thrown.
  * @param vueElement Vue Element: In this context it will be: this.$el
- * @param possibleOptions Array that is checked if it is in the array
- * @param actualValue Value that is checked if it is in the array
- * @param valueName Name for better error message. Describes which value was set incorrectly
+ * @param possibleOptions: An array of possible options.
+ * @param actualValue: A value to check for inclusion in the possibleOptions array.
+ * @param valueName: Name for better error message. Describes which value was set incorrectly
  */
 const checkPassedValue = (
   vueElement: Element,
